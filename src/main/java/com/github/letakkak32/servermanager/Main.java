831e7aec14d21b46d6f4d6f66bb7986665a813f7
letakkak32 <letakkak32@naver.com>
2023-01-30 15:24:22 +0900

    입장,퇴장 메세지 변경

--- src/main/java/com/github/letakkak32/servermanager/Main.java
+++ src/main/java/com/github/letakkak32/servermanager/Main.java
@@ -1,17 +1,39 @@
 package com.github.letakkak32.servermanager;
 
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin
+{
 
     @Override
-    public void onEnable() {
+    public void onEnable()
+    {
         // Plugin startup logic
-
+        System.out.println(ChatColor.GREEN + "Hello, servermanager is enabled!");
     }
 
     @Override
-    public void onDisable() {
+    public void onDisable()
+    {
         // Plugin shutdown logic
+        System.out.println(ChatColor.RED + "See you, servermanager is disabled...");
+    }
+
+    @EventHandler
+    public void onjoin(PlayerJoinEvent event)
+    {
+        String playername = event.getPlayer().getName();
+        event.setJoinMessage(ChatColor.YELLOW + playername + ChatColor.WHITE + "님이 " + ChatColor.GREEN + "입장" + ChatColor.WHITE + "하셨습니다.");
+    }
+
+    @EventHandler
+    public void onquit(PlayerQuitEvent event)
+    {
+        String playername = event.getPlayer().getName();
+        event.setQuitMessage(ChatColor.YELLOW + playername + ChatColor.WHITE + "님이 " + ChatColor.RED + "입장" + ChatColor.WHITE + "하셨습니다.");
     }
 }
