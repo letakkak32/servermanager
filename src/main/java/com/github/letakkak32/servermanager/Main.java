6cc6fddb35996a327a30ec2f01b7c6ced8374f03
letakkak32 <letakkak32@nate.com>
2023-03-29 16:57:27 +0900

    command testing build zzz

--- src/main/java/com/github/letakkak32/servermanager/Main.java
+++ src/main/java/com/github/letakkak32/servermanager/Main.java
@@ -2,19 +2,36 @@ package com.github.letakkak32.servermanager;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public class Main extends JavaPlugin
+public class Main extends JavaPlugin implements CommandExecutor
 {
     @Override
     public void onEnable()
     {
         Bukkit.getConsoleSender().sendMessage( ChatColor.AQUA + "플러그인이 활성화되었습니다.ㅋ");
     }
-
     @Override
     public void onDisable()
     {
         Bukkit.getConsoleSender().sendMessage( ChatColor.RED + "플러그인이 비활성화되었습니다.ㅠ");
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args)
+    {
+        switch(label)
+        {
+            case "testing":
+            {
+                sender.sendMessage("command testing complete! zzz");
+                break;
+            }
+        }
+        return true;
     }
 }
